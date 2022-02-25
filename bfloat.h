5485ab56ecad8289898ff80bdec97bc3765e2111
Rutayan Patro <59845040+rutayan@users.noreply.github.com>
2022-02-25 12:28:53 -0500

    bfloat to float helper done

--- bfloat.h
+++ bfloat.h
@@ -1,6 +1,6 @@
 #include <cstdint>
 #include <limits>
-#include <ostream>
+#include <iostream>
 #include <cstring>
 
 template <unsigned NumSignificandBits = 11>
@@ -10,6 +10,8 @@ public:
    * @brief default constructor does not initialize the value
    */
   BFloat() : value_(0) {}
+
+  BFloat(float input): value_(bfloatFromFloat(input)) {}
 
   std::uint32_t getBits() {
       std::uint32_t bits;
@@ -25,14 +27,14 @@ private:
     std::uint32_t bits;
     std::uint32_t bitsNext = bits + halfValue_;
 
-    bool halfwayAndEven = (bits & 0x1FFFF) == halfValue_; // FIX THIS
+    bool halfwayAndEven = (bits & halfwayEvenMask_) == halfValue_;
     bool signChange = ((bits ^ bitsNext) & (1 << 31)) > 0;
 
     if (halfwayAndEven || signChange) {
       bitsNext = bits;
     }
 
-    std::uint32_t resultBits = bitsNext >> numFlushBits_; // FIX THIS
+    std::uint32_t resultBits = bitsNext >> numFlushBits_;
     
     float result;
     std::memcpy(&result, &resultBits, sizeof(result));
@@ -45,9 +47,12 @@ private:
   static constexpr unsigned numExponentBits_ = 8;
 
   static constexpr unsigned halfValue_ = 1 << (maxSignificandBits_ - NumSignificandBits - 1);
-  static constexpr unsigned numFlushBits_ = std::numeric_limits<float>::digits - numSignBits_ + numExponentBits_ + NumSignificandBits;
+  static constexpr unsigned numFlushBits_ = std::numeric_limits<std::uint32_t>::digits - (numSignBits_ + numExponentBits_ + NumSignificandBits);
+  static constexpr unsigned halfwayEvenMask_ = (1 << (numFlushBits_ + 1)) - 1;
 };
 
 int main(){
+    BFloat<19> bf(2.f);
+
     return 0;
 }
